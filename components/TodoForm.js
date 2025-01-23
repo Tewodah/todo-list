32dd6c16fd23f4f4905ef6f61f436cb1817bffd0
Tewodah <tewodahnaz7@gmail.com>
2025-01-23 15:11:47 +0300

    `Updated TodoForm and TodoList components with image backgrounds and styling changes`

--- components/TodoForm.js
+++ components/TodoForm.js
@@ -1,18 +1,18 @@
 
-import React,{useState} from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, ImageBackground } from "react-native";
 import { useSelector } from "react-redux";
 
 import { addTodo } from "../lib/features/TodoList/service";
 
-export default function TodoForm({navigation}) {
-  const {loading, error}= useSelector((state)=> state.todoList);
-  const [todo,setTodo]=React.useState("");
+export default function TodoForm({ navigation }) {
+  const { loading, error } = useSelector((state) => state.todoList);
+  const [todo, setTodo] = React.useState("");
 
-  function saveTodo (params){
+  function saveTodo(params) {
     addTodo({
-      title:todo,
-      completed : false ,
+      title: todo,
+      completed: false,
     });
     navigation.goBack();
 
@@ -23,63 +23,72 @@ export default function TodoForm({navigation}) {
   if (error === true) {
     return <Text style={styles.error}>error...</Text>
   }
-  
 
-  
+
+
   return (
     <>
       <View style={styles.container}>
+        <ImageBackground source={require('../assets/R.jpg')} style={{ width: '100%', height: '30%' }} />
         <Button
           style={styles.backButton}
-          title="GO Back"
+          title="Go Back"
           onPress={() => navigation.goBack()}
         />
-        <Text style={styles.title}>Todolist Form</Text>
-        <TextInput placeholder="search" onChangeText={setTodo} style={styles.input}></TextInput>
-        <Button title= "save Todo" onPress={saveTodo}/>
+        <Text style={styles.title}>Todo List - Form</Text>
+        <TextInput placeholder="write your tasks here..." onChangeText={setTodo} style={styles.input}></TextInput>
+        <Button title="add Task" onPress={saveTodo} />
       </View>
     </>
   );
 
 }
 const styles = StyleSheet.create({
-  loading:{
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+
+  loading: {
     color: "#fff",
     backgroundColor: "#F0F0F0",
-    Text: "#3333333",
     width: 400,
     padding: 10,
   },
- error:{
+  error: {
     color: "#fff",
     backgroundColor: "#F0F0F0",
-    Text: "#3333333",
     width: 400,
     padding: 10,
   },
   backButton: {
     color: "#fff",
     backgroundColor: "#F0F0F0",
-    Text: "#3333333",
     width: 400,
     padding: 10,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#b0b6fc",
-    alignItems: "center",
+
+
   },
   title: {
-    fontSize: 60,
+    padding: 30,
+    fontSize: 30,
     fontWeight: "600",
     fontFamily: "gothic, sans-serif",
+    textAlign: "center",
   },
   input: {
-    color: "#fff",
-    backgroundColor: "black",
+    color: "BLACK",
+    backgroundColor: "GRAY",
     Text: "#3333333",
-    width: 400,
     padding: 10,
     margin: 25,
+    textAlign: "start",
+    boxShadow: "0 0 10px 0 rgba(48, 14, 238, 0.6)",
+  },
+  saveTodo: {
+    color: "#fff",
+    Text: "#3333333",
+    width: 400,
+    padding: 60,
   },
 });
--- components/TodoList.js
+++ components/TodoList.js
@@ -1,65 +1,67 @@
 import { StatusBar } from "expo-status-bar";
-import {useEffect} from "react";
-import {StyleSheet,Text, View,TextInput,ScrollView, Button, TouchableOpacity,} from "react-native";
+import { useEffect } from "react";
+import { ImageBackground, StyleSheet, Text, View, TextInput, ScrollView, Button, TouchableOpacity, } from "react-native";
 import { useSelector } from "react-redux";
 import { error, loading } from "../lib/features/TodoList/reducers";
-import { getTodos,  updateTodoStatus , deleteTodo} from "../lib/features/TodoList/service";
+import { getTodos, updateTodoStatus, deleteTodo } from "../lib/features/TodoList/service";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 
 export default function TodoList({ navigation }) {
-  const {todoList , loading , error } = useSelector((state)=> state.todoList);
+  const { todoList, loading, error } = useSelector((state) => state.todoList);
 
-function changeTodostatus(todo){
+  function changeTodostatus(todo) {
 
-  updateTodoStatus({id: todo.id , completed : !todo.completed});
+    updateTodoStatus({ id: todo.id, completed: !todo.completed });
 
-}
+  }
 
 
 
-  useEffect(()=> {
-  getTodos();
- },[]);
+  useEffect(() => {
+    getTodos();
+  }, []);
 
-if (loading ===true){
-  return <Text style={styles.loading}>loading...</Text>
-}
-if (error ===true){
-  return <Text style={styles.error}>error...</Text>
-}
+  if (loading === true) {
+    return <Text style={styles.loading}>loading...</Text>
+  }
+  if (error === true) {
+    return <Text style={styles.error}>error...</Text>
+  }
 
   return (
     <View style={styles.container}>
+      <ImageBackground source={require('../assets/R.jpg')} style={{ width: '100%', height: '30%' }} />
+
+      <Text style={styles.title}>Todo List 2025</Text>
       <Button
         style={styles.addTodo}
-        title="Add Todo"
+        title="Add Task"
         onPress={() => navigation.navigate("todoForm")}
       />
-      <Text style={styles.title}>Todo-Lists</Text>
       <TextInput placeholder="search" style={styles.input}></TextInput>
       <ScrollView>
         {todoList.map((todo, i) => (
-          <View style={styles.todocontainer} key={i}>
-          <Text style = {styles.todoText}>{todo.title} </Text>
-          <TouchableOpacity onPress={() => changeTodostatus(todo)}>
-          <MaterialCommunityIcons 
-          name={todo.completed? "checkbox-marked":"checkbox-blank"} 
-          size={40} 
-          color= { todo.completed? "green" :"gray" }
-          style = {{paddingHorizontal :30}}
-          />
-          </TouchableOpacity>
+          <View style={styles.todoContainer} key={i}>
+            <Text style={styles.todoText}>{todo.title} </Text>
+            <TouchableOpacity onPress={() => changeTodostatus(todo)}>
+              <MaterialCommunityIcons
+                name={todo.completed ? "checkbox-marked" : "checkbox-blank"}
+                size={40}
+                color={todo.completed ? "green" : "white"}
+                style={{ paddingHorizontal: 30 }}
+              />
+            </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => deleteTodo({id:todo.id})}>
-          <MaterialCommunityIcons 
-          name="delete"
-          size={40} 
-          color=  "red"
-          style = {{paddingHorizontal :30}}
-          />
-          </TouchableOpacity>
-       
+            <TouchableOpacity onPress={() => deleteTodo({ id: todo.id })}>
+              <MaterialCommunityIcons
+                name="delete"
+                size={40}
+                color="red"
+                style={{ paddingHorizontal: 30 }}
+              />
+            </TouchableOpacity>
+
           </View>
         ))}
       </ScrollView>
@@ -68,14 +70,14 @@ if (error ===true){
 }
 
 const styles = StyleSheet.create({
-  loading:{
+  loading: {
     color: "#fff",
     backgroundColor: "#F0F0F0",
     Text: "#3333333",
     width: 400,
     padding: 10,
   },
- error:{
+  error: {
     color: "#fff",
     backgroundColor: "#F0F0F0",
     Text: "#3333333",
@@ -84,43 +86,49 @@ const styles = StyleSheet.create({
   },
   addTodo: {
     color: "#fff",
-    backgroundColor: "#fff",
+    backgroundColor: "#FE9900",
     Text: "#3333333",
-    width: 400,
-    padding: 10,
+
   },
   container: {
     flex: 1,
-    backgroundColor: "gray",
-    fontSize: 100,
     alignItems: "center",
   },
   title: {
-    fontSize: 60,
+    fontSize: 50,
+    padding: 20,
+    color: "black",
+    transparent: 0.5,
     fontWeight: "600",
     fontFamily: "gothic, sans-serif",
   },
   input: {
-    color: "#fff",
-    backgroundColor: "#000",
+    color: "black",
+    Textsize: 40,
+    backgroundColor: "GRAY",
+    fontWeight: "600",
     Text: "#3333333",
-    width: 400,
     padding: 10,
-    margin: 25,
+    width: 610,
+    textAlign: "start",
+    boxShadow: "0 0 10px 0 rgba(48, 14, 238, 0.6)",
   },
-  
-  todocontainer:{
- flexDirection: "row",
- alignItems: "center",
- justifyContent:"space-between",
- color: "#000",
-  backgroundColor: "white",
+
+  todoContainer: {
+
+    backgroundColor: "#358AEC",
+    flexDirection: "row",
+    alignContent: "flex-end",
+    color: "#000",
+    borderRadius: 10,
+
   },
 
   todoText: {
     Text: "#3333333",
     width: 400,
     padding: 20,
-   
+
+
   },
 });
